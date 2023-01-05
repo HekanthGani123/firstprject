6c77888f727cfca654ab3f489901c4f3979894b7
HekanthGani123 <103807358+HekanthGani123@users.noreply.github.com>
2023-01-05 17:19:13 +0530

    Update DriveComparator.java

--- Collection FrameWork/src/com/comparator/DriveComparator.java
+++ Collection FrameWork/src/com/comparator/DriveComparator.java
@@ -7,6 +7,8 @@ public class DriveComparator implements Comparator<Memory>{
 	@Override
 	public int compare(Memory o1, Memory o2) {
 		return o1.getDrive().compareTo(o2.getDrive());
+		System.out.println("hi");
+				   
 	}
 
 }
